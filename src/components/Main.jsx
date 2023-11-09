fa073a3e74f91bbad6a4f41f99d94599cb0b68ce
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-11-08 21:30:13 -0500

    added icons for links and title

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import { FaLinkedinIn, FaStackOverflow, FaGithub } from "react-icons/fa";
 
 const Main = () => {
   return (
@@ -10,8 +11,9 @@ const Main = () => {
         alt="An image of a body of water surrounded by foliage and sheer rock"
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/60">
-        <h1>I'm Aaron Santowasso, Welcome to my site!</h1>
-        <h2>
+        <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+        <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Aaron Santowasso</h1>
+        <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
           I'm a
           <TypeAnimation
             sequence={[
@@ -27,10 +29,20 @@ const Main = () => {
             ]}
             wrapper="span"
             speed={50}
-            style={{ fontSize: "1em", display: "inline-block", paddingLeft: '5px' }}
+            style={{
+              fontSize: "1em",
+              display: "inline-block",
+              paddingLeft: "5px",
+            }}
             repeat={Infinity}
           />
         </h2>
+        <div className='flex justify-between pt-6 max-w-[200px] w-full'>
+          <FaLinkedinIn className='cursor-pointer' size={20} />
+          <FaStackOverflow className='cursor-pointer' size={20} />
+          <FaGithub className='cursor-pointer' size={20} />
+        </div>
+        </div>
       </div>
     </div>
   );
