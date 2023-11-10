82e9a26982114f0aea281cfdfe477f68e898c88f
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-11-09 21:35:51 -0500

    filled out work history

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
-import Workitem from './Workitem'
+import React from "react";
+import Workitem from "./Workitem";
+
 
 const data = [
   {
     year: 2022,
-    title: "Campus Technician",
+    title: "Campus Technician, Compucom",
     duration: "to present",
-    details: "Sample sample sample"
-  }
-]
+    details: "Resolve software and hardware issues for clients. Assist new hires in obtaining and configuring the correct programs and tools. Keep up to date with the latest procedures to best serve customers.",
+  },
+
+  {
+    year: 2022,
+    title: "Marketing & Communications Intern, SOAR365",
+    duration: "to May 2022",
+    details: "Used print and digital resources to create a history of the organization. Synthesize information onto written material appropriate for the public that informs them about the organization. Coordinated with staff members both in person and remotely.",
+  },
+
+  {
+    year: 2021,
+    title: "IT Contractor, Strategic Systems",
+    duration: "to May 2022",
+    details: "Configure workstations at office locations according to client needs. Arrive on time to worksites and finish tasks in a timely manner.",
+  },
+
+  {
+    year: 2020,
+    title: "Communications Intern",
+    duration: "to December 2020",
+    details: "Research Virginia history topics to create material to educate the public using WordPress. Filmed and edited videos for social media. Met in person and remotely with staff members to plan the release of media and promotional material for in-person and online events.",
+  },
+];
 
 const Work = () => {
   return (
-    <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-      <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
-      {data.map((item, idx)=> (
-       <Workitem key={idx} year={item.year} title={item.title} duration={item.duration} details={item.details}/>
+    <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Work and Internships</h1>
+      {data.map((item, idx) => (
+        <Workitem
+          key={idx}
+          year={item.year}
+          title={item.title}
+          duration={item.duration}
+          details={item.details}
+        />
+        
+
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
