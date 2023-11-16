d1c874fd66ac49d9e02c77fc4de0e0c2e5a87010
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-11-15 21:39:03 -0500

    fixed display of project info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,12 +14,12 @@ const Projects = () => {
       </h1>
       
       <p className="text-center py-8">
-        Sample text sample text sample text sample text sample text sample text
+        A selection of projects showcasing my web development skills
       </p>
       <div className="grid sm:grid-cols-2 gap-12">
-        <Projectitem img={Vet} title="Pet-to-Vet" url="https://github.com/Asantowasso/pet-to-vet"/>
+        <Projectitem img={Vet} desc="Users can create a profile for thier pet to be viewed by a veterinarian" title="Pet-to-Vet" url="https://github.com/Asantowasso/pet-to-vet" deployed="https://damp-forest-39395.herokuapp.com/login"/>
 
-        <Projectitem img={Solar} title="Solar System" url="https://github.com/Asantowasso/planets-and-moons" />
+        <Projectitem img={Solar} desc="A showcase of earth's solar system and various attributes of the planets" title="Solar System" url="https://github.com/Asantowasso/planets-and-moons" deployed="https://asantowasso.github.io/planets-and-moons/" />
 
         <Projectitem img={MET} title="MET Guide"  />
         
