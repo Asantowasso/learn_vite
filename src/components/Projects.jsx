9698a3e4963522ee901b56abfc42ccaff290c4e7
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-11-16 21:57:50 -0500

    added a contact component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
         A selection of projects showcasing my web development skills
       </p>
       <div className="grid sm:grid-cols-2 gap-12">
-        <Projectitem img={Vet} desc="Users can create a profile for thier pet to be viewed by a veterinarian" title="Pet-to-Vet" url="https://github.com/Asantowasso/pet-to-vet" deployed="https://damp-forest-39395.herokuapp.com/login"/>
+        <Projectitem img={Vet} desc="Users can create a profile for their pet to be viewed by a veterinarian" title="Pet-to-Vet" url="https://github.com/Asantowasso/pet-to-vet" deployed="https://damp-forest-39395.herokuapp.com/login"/>
 
         <Projectitem img={Solar} desc="A showcase of earth's solar system and various attributes of the planets" title="Solar System" url="https://github.com/Asantowasso/planets-and-moons" deployed="https://asantowasso.github.io/planets-and-moons/" />
 
