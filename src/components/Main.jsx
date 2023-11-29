cab34650ee3d8598fbcf134d069276ec3ce70e91
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-11-28 21:46:01 -0500

    changed styling of main icons and text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,10 +12,10 @@ const Main = () => {
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-[white/60]">
         <div className="max-w-[700px]   m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
+          <h1 className="sm:text-5xl text-4xl font-bold text-[white]">
             Aaron Santowasso
           </h1>
-          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
+          <h2 className="flex sm:text-3xl text-[white] text-2xl pt-4 ">
             I'm a
             <TypeAnimation
               sequence={[
@@ -35,14 +35,15 @@ const Main = () => {
                 fontSize: "1em",
                 display: "inline-block",
                 paddingLeft: "5px",
+                color: "white",
               }}
               repeat={Infinity}
             />
           </h2>
           <div className="flex justify-between  pt-6 max-w-[200px] w-full">
-            <FaLinkedinIn className="cursor-pointer" size={20} />
-            <FaStackOverflow className="cursor-pointer" size={20} />
-            <FaGithub className="cursor-pointer" size={20} />
+            <FaLinkedinIn className="cursor-pointer text-[white] " size={20} />
+            <FaStackOverflow className="cursor-pointer text-[white]" size={20} />
+            <FaGithub className="cursor-pointer text-[white]" size={20} />
           </div>
         </div>
       </div>
