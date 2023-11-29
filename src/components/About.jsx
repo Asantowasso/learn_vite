e2571a703682bdd59a5e0187d1991252a21f4c1f
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-11-28 22:14:25 -0500

    revised about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div id="about" className="max-w-[1040px] bg-[#252525] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl font-bold text-center text-[white]">About</h1>
-      <p className="text-center text-[white] py-8"> A multi-talented professional skilled in Information Technology, Web Development, Communication, Teamwork, Writing and Research. Currently employed as an IT professional with CompuCom where I have worked with clients such as Truist and Altria. Experience working on multimedia projects for non-profits on topics such as history and awareness around disabilities. Graduated from Randolph Macon College with a Bachelor's degree in history. Also completed a six month course on web development through the University of Richmond. 
+      <p className="text-center text-[white] py-8"> A multi-talented professional skilled in Information Technology, Web Development, Communication, Teamwork, Writing and Research. Currently employed as an IT professional with CompuCom where I have worked with clients such as Truist and Altria. Experience working on multimedia projects for non-profits on topics such as history and awareness around disabilities. Graduated from Randolph Macon College with a Bachelor's degree in history.
     
 
       </p>
